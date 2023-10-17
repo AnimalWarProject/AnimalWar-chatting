ab057c36fc5f74dc55ea43d56735dd3ca4898f25
sein <lsi6601@gmail.com>
2023-10-17 15:37:55 +0900

    init

---
+++ src/main/java/com/example/animalwarchatting/AnimalWarChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalwarchatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalWarChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalWarChattingApplication.class, args);
+    }
+
+}
